9e4b17157228fc612a267424545007206fd86788
Amine Mersoul <amersoul@e2r6p1.1337.ma>
2018-11-27 12:36:52 +0100

    gesture control

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:material_page_reveal/page_dragger.dart';
 import 'package:material_page_reveal/page_reveal.dart';
 import 'package:material_page_reveal/pager_indicator.dart';
 import 'package:material_page_reveal/pages.dart';
@@ -27,29 +30,72 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  StreamController<SlideUpdate> slideUpdateStream;
+
+  int activeIndex = 0;
+  int nextPageIndex = 0;
+  SlideDirection slideDirection = SlideDirection.none;
+  double slidePercent = 0.0;
+
+  _MyHomePageState() {
+    slideUpdateStream = StreamController<SlideUpdate>();
+
+    slideUpdateStream.stream.listen((SlideUpdate event) {
+      setState(() {
+        if (event.updateType == UpdateType.dragging)
+        {
+          slideDirection = event.direction;
+          slidePercent = event.slidePercent;
+
+          if (slideDirection == SlideDirection.leftToRight) {
+            nextPageIndex = activeIndex - 1;
+          } else if (slideDirection == SlideDirection.rightToLeft) {
+            nextPageIndex = activeIndex + 1;
+          } else {
+            nextPageIndex = activeIndex;
+          }
+        } else if (event.updateType == UpdateType.doneDragging) {
+          if (slidePercent > 0.5) {
+            activeIndex = slideDirection == SlideDirection.leftToRight
+            ? activeIndex - 1
+            : activeIndex + 1;
+          }
+          slideDirection = SlideDirection.none;
+          slidePercent = 0.0;
+        }
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: <Widget>[
           Page(
-            viewModel: pages[0],
+            viewModel: pages[activeIndex],
             percentVisible: 1.0,
           ),
           PageReveal(
-            revealPercent: 1.0,
+            revealPercent: slidePercent,
             child: Page(
-              viewModel: pages[1],
-              percentVisible: 1.0,
+              viewModel: pages[nextPageIndex],
+              percentVisible: slidePercent,
             ),
           ),
           PagerIndicator(
             viewModel: PagerIndicatorViewModel(
               pages,
-              1,
-              SlideDirection.leftToRight,
-              1.0
+              activeIndex,
+              slideDirection,
+              slidePercent
             ),
+          ),
+          PageDragger(
+            canDragLeftToRight: activeIndex > 0,
+            canDragRightToLeft: activeIndex < pages.length - 1,
+            slideUpdateStream: slideUpdateStream,
           ),
         ],
       ),
